a9547dc377d715af20b6072c2bcc3b231e228946
Thales Machado <thales.mchd@gmail.com>
2015-10-17 12:56:58 -0300

    map reader working

---
+++ aima-core/src/main/java/aima/core/environment/map/mapfiles/MapReader.java
@@ -0,0 +1,99 @@
+package aima.core.environment.map.mapfiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidParameterException;
+import java.util.IllegalFormatException;
+
+public class MapReader {
+	
+	public interface MapReaderCallback {
+		void onMapName(String mapName);
+		void onNewDistance(String locationName, int distanceToReferencePosition, int bearingDirections);
+		void onDirectionalLink(String originName, String destinyName, int distance);
+	}
+	
+	public static enum Map {
+		ROMANIA("PartOfRomania.txt");
+		
+		private final String value;
+		Map(String value) {
+			this.value = value;
+		}
+		
+		public String getName() {
+			return this.value;
+		}
+	};
+	
+	public int getNumberOfMaps() {
+		return Map.values().length;
+	}
+	
+	private String getNameOfMapFile(int position) {
+		if (position < 0 || position > getNumberOfMaps()) {
+			throw new NullPointerException();
+		}
+		
+		return Map.values()[position].getName();
+	}
+
+	public void readFile(int position, MapReaderCallback callback) {
+		if (callback == null) {
+			throw new InvalidParameterException();
+		}
+		
+		URL url = getClass().getResource(getNameOfMapFile(position));
+		BufferedReader br = null;
+		
+		
+		int currentLine = 0;
+		int cityCounter = 0;
+		int numberOfcities = 0;
+		int routesCounter = 0;
+		int numberOfRoutes = 0;
+		try {
+			
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(url.getPath()));
+			while ((sCurrentLine = br.readLine()) != null) {
+				
+				if (currentLine == 0) {
+					callback.onMapName(sCurrentLine);
+				} else if (currentLine == 1) {
+					numberOfcities = Integer.valueOf(sCurrentLine);
+				} else if (currentLine - 2 < numberOfcities) {
+					String[] params = sCurrentLine.split(" ");
+					callback.onNewDistance(params[0], Integer.valueOf(params[1]), Integer.valueOf(params[2]));
+					cityCounter++;
+				} else if (currentLine - 2 == numberOfcities) {
+					numberOfRoutes = Integer.valueOf(sCurrentLine);
+				} else {
+					System.out.println(sCurrentLine);
+					routesCounter++;
+				}
+				
+				currentLine++;
+			}
+			
+			if (numberOfRoutes != routesCounter) {
+				throw new IllegalArgumentException("Number of routes provided and actual routes provided differs. Please fix file: " + getNameOfMapFile(position));
+			} else if (numberOfcities != cityCounter) {
+				throw new IllegalArgumentException("Number of cities provided and actual cities provided differs. Please fix file: " + getNameOfMapFile(position));
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+}
--- aima-gui/src/main/java/aima/gui/applications/AimaDemoApp.java
+++ aima-gui/src/main/java/aima/gui/applications/AimaDemoApp.java
@@ -1,5 +1,7 @@
 package aima.gui.applications;
 
+import aima.core.environment.map.mapfiles.MapReader;
+import aima.core.environment.map.mapfiles.MapReader.MapReaderCallback;
 import aima.gui.applications.search.csp.MapColoringApp;
 import aima.gui.applications.search.games.ConnectFourApp;
 import aima.gui.applications.search.games.EightPuzzleApp;
@@ -65,5 +67,24 @@ public class AimaDemoApp {
 		registerDemos(frame);
 		frame.setSize(800, 600);
 		frame.setVisible(true);
+		
+		MapReader reader = new MapReader();
+		reader.readFile(0, new MapReaderCallback() {
+			
+			@Override
+			public void onNewDistance(String locationName, int distanceToReferencePosition, int bearingDirections) {
+				System.out.println("Location: " + locationName + " { " + distanceToReferencePosition + ", " + bearingDirections + " }");
+			}
+			
+			@Override
+			public void onMapName(String mapName) {
+				System.out.println("Map name: " + mapName);
+			}
+			
+			@Override
+			public void onDirectionalLink(String originName, String destinyName, int distance) {
+				System.out.println("Route from " + originName + " to " + destinyName + ": " + distance);
+			}
+		});
 	}
 }
